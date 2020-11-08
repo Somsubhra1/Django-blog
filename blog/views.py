eb4c409a9f9f2847d1391dbadfd149a07356ed4b
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-08 22:43:32 +0530

    blog templates and static assets added

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': "Somsubhra Das",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "Aug 27, 2019"
+    },
+    {
+        'author': "Jane Doe",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": "Aug 17, 2019"
+    },
+
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
