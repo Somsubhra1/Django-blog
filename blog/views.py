df66a946bddd71dda2e2958e787d62e656de9a75
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-09 23:01:05 +0530

    post model created

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-
-posts = [
-    {
-        'author': "Somsubhra Das",
-        "title": "Blog post 1",
-        "content": "First post content",
-        "date_posted": "Aug 27, 2019"
-    },
-    {
-        'author': "Jane Doe",
-        "title": "Blog post 2",
-        "content": "Second post content",
-        "date_posted": "Aug 17, 2019"
-    },
-
-]
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
