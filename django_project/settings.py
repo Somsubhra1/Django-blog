2b911d9d12b7a2927a9ebde546701b0170c485c5
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-11 12:05:06 +0530

    added .env file

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y)rx5x%c6zcqf265h-stn+yl4x3a04ghx-^%&jf@9g1cidf7e2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get("DJANGO_ENV") == "development" else False
 
 ALLOWED_HOSTS = []
 
