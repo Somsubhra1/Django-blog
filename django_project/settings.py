5591aaba99b36dfeb5d2c19b39ef0e11fbb09c20
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-13 22:12:47 +0530

    user registration form done

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog",
-    "users"
+    "users",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
@@ -8,13 +8,13 @@ from django.contrib import messages
 def register(request):
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {username}!")
             return redirect("blog-home")
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, "users/register.html", {"form": form})
