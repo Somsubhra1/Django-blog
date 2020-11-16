0b9e77c54d55666042e6476281047ad40dea6878
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-16 14:33:24 +0530

    login and logout templates done

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,3 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = "blog-home"
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", include("blog.urls")),
     path("register/", users_views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"),
+         name="login"),  # Change template lookup directory
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('admin/', admin.site.urls),
 ]
