a34ecad39816e3e4ab2742931780edd05315a1ce
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-14 00:02:54 +0530

    modified allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DJANGO_ENV") == "development" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
