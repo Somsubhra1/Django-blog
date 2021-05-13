b55028f2fa1a8c03137291fc7eb3d44048e1e44e
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-13 23:51:28 +0530

    static directory set

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+
 # directory in which files are stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
