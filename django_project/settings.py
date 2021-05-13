d255374697c513410f3788f75489b38df4637251
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-13 23:57:01 +0530

    static files collected

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "blog", "static", "blog")
 ]
 
 
